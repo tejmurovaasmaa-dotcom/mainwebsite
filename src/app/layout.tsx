f60ac5b25f89c3128be156b379e3718eeb7339d0
trustbridge225 <trustbridge225@gmail.com>
2026-02-18 17:54:04 +0530

    added gtm in root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -36,7 +36,27 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={`${cinzel.variable} ${montserrat.variable}`} >
+      <head>
+         {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+           new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+           j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+           'https://www.googletagmanager.com/gtm.js?id=GTM-TW83VV5S'+dl;f.parentNode.insertBefore(j,f);
+           })(window,document,'script','dataLayer','GTM-TW83VV5S');`
+          }}
+        />
+      </head>
       <body>
+         <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-TW83VV5S"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
         {children}
       </body>
     </html>
